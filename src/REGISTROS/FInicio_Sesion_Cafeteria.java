9f493cb9a88d6e91220ef4104affbbf2bbe2332c
Usuario <Usuario@Luffy>
2025-04-03 07:18:14 -0500

    actualizar1

--- src/REGISTROS/FInicio_Sesion_Cafeteria.java
+++ src/REGISTROS/FInicio_Sesion_Cafeteria.java
@@ -67,7 +67,14 @@ public class FInicio_Sesion_Cafeteria extends javax.swing.JFrame {
         btnproducto = new javax.swing.JButton();
         btnnutrientes = new javax.swing.JButton();
         btnventas = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
+        btndetalleventa = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         menu = new javax.swing.JButton();
         ancla1 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
@@ -104,7 +111,7 @@ public class FInicio_Sesion_Cafeteria extends javax.swing.JFrame {
         ancla.setPreferredSize(new java.awt.Dimension(1000, 1000));
 
         pnmenu.setBackground(new java.awt.Color(112, 138, 147));
-        pnmenu.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(javax.swing.border.EtchedBorder.RAISED, new java.awt.Color(0, 0, 0), new java.awt.Color(0, 0, 0)), "MENÚ", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Showcard Gothic", 1, 36), new java.awt.Color(0, 0, 0))); // NOI18N
+        pnmenu.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createEtchedBorder(javax.swing.border.EtchedBorder.RAISED, new java.awt.Color(0, 0, 0), new java.awt.Color(0, 0, 0)), "MENÚ", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Showcard Gothic", 1, 36), new java.awt.Color(0, 0, 0))); // NOI18N
 
         btnempleados.setBackground(new java.awt.Color(121, 182, 201));
         btnempleados.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
@@ -175,49 +182,99 @@ public class FInicio_Sesion_Cafeteria extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ICONOS/punto-de-venta.png"))); // NOI18N
-        jButton1.setToolTipText("DETALLE DE VENTAS");
-        jButton1.setContentAreaFilled(false);
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btndetalleventa.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ICONOS/punto-de-venta.png"))); // NOI18N
+        btndetalleventa.setToolTipText("DETALLE DE VENTAS");
+        btndetalleventa.setContentAreaFilled(false);
+        btndetalleventa.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btndetalleventa.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btndetalleventaActionPerformed(evt);
             }
         });
+
+        jLabel2.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel2.setText("CLIENTES");
+
+        jLabel1.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel1.setText("EMPLEADOS");
+
+        jLabel4.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel4.setText("FAMILIAS");
+
+        jLabel6.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel6.setText("NUTRIENTES");
+
+        jLabel7.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel7.setText("DETA-VENTA");
+
+        jLabel8.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel8.setText("VENTAS");
+
+        jLabel5.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel5.setText("PRODUCTOS");
 
         javax.swing.GroupLayout pnmenuLayout = new javax.swing.GroupLayout(pnmenu);
         pnmenu.setLayout(pnmenuLayout);
         pnmenuLayout.setHorizontalGroup(
             pnmenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(pnmenuLayout.createSequentialGroup()
-                .addGap(15, 15, 15)
+                .addContainerGap()
                 .addGroup(pnmenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(btnventas)
+                    .addComponent(jLabel7)
                     .addComponent(btnclientes)
+                    .addComponent(jLabel2)
                     .addComponent(btnempleados)
+                    .addComponent(jLabel1)
                     .addComponent(btnfamilia)
+                    .addComponent(jLabel4)
                     .addComponent(btnproducto)
+                    .addComponent(jLabel5)
                     .addComponent(btnnutrientes)
-                    .addComponent(jButton1))
-                .addContainerGap(23, Short.MAX_VALUE))
+                    .addComponent(jLabel6)
+                    .addComponent(btndetalleventa)
+                    .addComponent(btnventas)
+                    .addComponent(jLabel8))
+                .addContainerGap(16, Short.MAX_VALUE))
         );
         pnmenuLayout.setVerticalGroup(
             pnmenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(pnmenuLayout.createSequentialGroup()
+                .addContainerGap()
                 .addComponent(btnclientes)
-                .addGap(15, 15, 15)
-                .addComponent(btnempleados)
-                .addGap(15, 15, 15)
+                .addGap(5, 5, 5)
+                .addComponent(jLabel2)
+                .addGap(5, 5, 5)
+                .addComponent(btnempleados, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnfamilia)
-                .addGap(15, 15, 15)
+                .addGap(4, 4, 4)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnproducto)
-                .addGap(15, 15, 15)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel5)
+                .addGap(1, 1, 1)
                 .addComponent(btnnutrientes)
-                .addGap(15, 15, 15)
-                .addComponent(jButton1)
-                .addGap(15, 15, 15)
+                .addGap(5, 5, 5)
+                .addComponent(jLabel6)
+                .addGap(5, 5, 5)
+                .addComponent(btndetalleventa)
+                .addGap(5, 5, 5)
+                .addComponent(jLabel7)
+                .addGap(5, 5, 5)
                 .addComponent(btnventas)
-                .addContainerGap(75, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel8)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         menu.setBackground(new java.awt.Color(209, 235, 247));
@@ -256,28 +313,29 @@ public class FInicio_Sesion_Cafeteria extends javax.swing.JFrame {
                 .addComponent(pnmenu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGroup(anclaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(anclaLayout.createSequentialGroup()
-                        .addGap(14, 14, 14)
+                        .addGap(28, 28, 28)
                         .addComponent(menu)
                         .addGap(399, 399, 399)
                         .addComponent(jLabel3))
                     .addGroup(anclaLayout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(ancla1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(15, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         anclaLayout.setVerticalGroup(
             anclaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(anclaLayout.createSequentialGroup()
-                .addGroup(anclaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(pnmenu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(anclaLayout.createSequentialGroup()
-                        .addGap(12, 12, 12)
-                        .addGroup(anclaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addComponent(menu))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(ancla1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(11, Short.MAX_VALUE))
+                .addGap(12, 12, 12)
+                .addGroup(anclaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3)
+                    .addComponent(menu))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(ancla1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(43, 43, 43))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, anclaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(pnmenu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -288,7 +346,7 @@ public class FInicio_Sesion_Cafeteria extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(ancla, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 754, Short.MAX_VALUE)
+            .addComponent(ancla, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 788, Short.MAX_VALUE)
         );
 
         pack();
@@ -346,11 +404,11 @@ public class FInicio_Sesion_Cafeteria extends javax.swing.JFrame {
         desplegar(p);
     }//GEN-LAST:event_btnventasActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btndetalleventaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btndetalleventaActionPerformed
         
         Panel_Detalle_Venta p = new Panel_Detalle_Venta();
         desplegar(p);
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btndetalleventaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -391,14 +449,21 @@ public class FInicio_Sesion_Cafeteria extends javax.swing.JFrame {
     private javax.swing.JPanel ancla;
     private javax.swing.JPanel ancla1;
     private javax.swing.JButton btnclientes;
+    private javax.swing.JButton btndetalleventa;
     private javax.swing.JButton btnempleados;
     private javax.swing.JButton btnfamilia;
     private javax.swing.JButton btnnutrientes;
     private javax.swing.JButton btnproducto;
     private javax.swing.JButton btnventas;
     private javax.swing.ButtonGroup buttonGroup1;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenu jMenu5;
